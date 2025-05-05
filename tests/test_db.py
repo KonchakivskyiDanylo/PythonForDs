6dd0687a6f671d08f88805b8a40c9c0cb863aca4
ArDangerUS <artemlotariev@gmail.com>
2025-05-05 12:40:10 +0300

    some tests to understand how it work

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,7 +1,7 @@
-import pymongo
 import mongomock
 
 def test_mongodb_connection():
     client = mongomock.MongoClient()
     db = client["PythonForDs"]
+    db["prediction"].insert_one({"sample": "data"})
     assert "prediction" in db.list_collection_names()
